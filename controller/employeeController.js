d80d93181e924b03cf555543b5a5dd388dcddc76
Dinesh <yadladinesh1@gmail.com>
2022-06-10 16:47:33 +0530

    fix update function

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -100,21 +100,31 @@ exports.empDelete = async (req, res) => {
 exports.empUpdate = async (req, res) => {
   try {
     var id = req.params.id;
-    employeeModel.updateOne(
-      {_id: id },
-      {
-        $set: {
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          dob: req.body.dob,
-          address: req.body.address,
+    employeeModel
+      .findOneAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            dob: req.body.dob,
+            address: req.body.address,
+          },
         },
-      },
-      {upsert: true},
-      function (err, result) {
-        console.log(result, "result");
-      }
-    );
+        { upsert: true }
+      )
+      .then((result) => {
+        res.status(200).json({
+          message: "Employee data updated",
+          data: result,
+        });
+      })
+      .catch((err) => {
+        res.status(404).json({
+          message: "error",
+          error: err.message,
+        });
+      });
   } catch (error) {
     res.status(500).json({
       message: "error",
